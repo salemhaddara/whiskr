881d5b8fffa020a8660202863674b5a83ede0a5e
Saad Ardati <saadodi44@gmail.com>
2024-07-06 11:14:25 +0300

    Add go router, .gitignore.

--- lib/router.dart
+++ lib/router.dart
@@ -1,9 +1,27 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:go_router/go_router.dart';
+import 'package:whiskr/main.dart';
+
+import 'auth.dart';
 
 final GoRouter router = GoRouter(
+  redirect: (context, state) {
+    final user = FirebaseAuth.instance.currentUser;
+
+    if (user == null) {
+      return '/login';
+    } else {
+      return '/home';
+    }
+  },
   routes: [
     GoRoute(
+      path: '/',
+      builder: (context, state) => const HomePage(),
+    ),
+    GoRoute(
       path: 'auth',
+      builder: (context, state) => const AuthPage(),
     ),
   ],
 );
