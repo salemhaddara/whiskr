b1a9ee6cd8f614731d26d1f02ea0dc440e6a9693
Merched kazan <kazanmerched@gmail.com>
2024-07-06 16:42:06 +0300

    edit profile details

--- lib/router.dart
+++ lib/router.dart
@@ -1,6 +1,4 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:go_router/go_router.dart';
-import 'package:whiskr/chats/conversations_screen.dart';
 import 'package:whiskr/view/home_page.dart';
 import 'package:whiskr/view/profile_details_page.dart';
 
@@ -9,15 +7,15 @@ import 'view/auth_page.dart';
 
 final GoRouter router = GoRouter(
   initialLocation: '/',
-  redirect: (context, state) {
-    final user = FirebaseAuth.instance.currentUser;
-
-    /*   if (user == null) {
-      return '/auth';
-    } else {
-      return null;
-    } */
-  },
+  // redirect: (context, state) {
+  //   final user = FirebaseAuth.instance.currentUser;
+  //
+  //   if (user == null) {
+  //     return '/auth';
+  //   } else {
+  //     return '/';
+  //   }
+  // },
   routes: [
     GoRoute(
       path: '/auth',
@@ -34,10 +32,6 @@ final GoRouter router = GoRouter(
           ),
         ),
       ]
-    ),
-    GoRoute(
-      path: '/chats',
-      builder: (context, state) => ConversationsScreen(),
     ),
   ],
 );
--- lib/view/profile_details_page.dart
+++ lib/view/profile_details_page.dart
@@ -1,5 +1,6 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:whiskr/model/profile.dart';
 
 class ProfileDetailsPage extends StatelessWidget {
@@ -11,6 +12,7 @@ class ProfileDetailsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Stack(
             children: [
@@ -22,6 +24,7 @@ class ProfileDetailsPage extends StatelessWidget {
                               image: DecorationImage(
                                 image: NetworkImage(e),
                                 fit: BoxFit.cover,
+
                               ),
                             ),
                           ))
@@ -29,52 +32,97 @@ class ProfileDetailsPage extends StatelessWidget {
                   options: CarouselOptions(
                       height: 300, viewportFraction: 1, autoPlay: true)),
               Container(
-                height: 300,
+                height: 75,
                 width: MediaQuery.of(context).size.width,
                 decoration: BoxDecoration(
                   gradient: LinearGradient(
                     colors: [
-                      Colors.blue.withOpacity(0.2),
-                      Colors.black.withOpacity(0.7),
+                      Colors.black.withOpacity(0.5),
+                      Colors.black.withOpacity(0),
                     ],
                     begin: Alignment.topCenter,
                     end: Alignment.bottomCenter,
                   ),
                 ),
-              )
+              ),
+              Positioned(
+                top: MediaQuery.of(context).padding.top,
+                left: 10,
+                child: BackButton(
+                  color: Colors.white,
+                ))
             ],
           ),
-          const SizedBox(
-            height: 12,
-          ),
-          Text(
-            profile.name,
-            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-          ),
-          const SizedBox(
-            height: 12,
-          ),
-          Text(
-            profile.bio,
-            style: const TextStyle(fontWeight: FontWeight.w500, fontSize: 16),
-          ),
-          const SizedBox(
-            height: 12,
-          ),
-          Text(
-            profile.dob.toString(),
-            style: const TextStyle(fontWeight: FontWeight.w400, fontSize: 14),
-          ),
-          const SizedBox(height: 12),
-          Text(
-            profile.type.name,
-            style: const TextStyle(fontWeight: FontWeight.w400, fontSize: 14),
-          ),
-          const SizedBox(
-            height: 12,
-          ),
-          ElevatedButton(onPressed: () {}, child: const Text("Start Chat"))
+        Padding(padding: const EdgeInsets.all(8),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(
+              height: 12,
+            ),
+            Text(
+              profile.name,
+              style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            ),
+            const SizedBox(
+              height: 12,
+            ),   Row(
+              children: [
+               const FaIcon(
+                  FontAwesomeIcons.birthdayCake
+                  ,size: 15,),
+               const SizedBox(width: 10,),
+                Text(
+                  profile.dob.toString(),
+                  style: const TextStyle(fontWeight: FontWeight.w400, fontSize: 14),
+                ),
+
+              ],
+            ),
+            const SizedBox(
+              height: 12,
+            ),
+           const Text("Bio:",style: TextStyle(fontSize: 15,fontWeight: FontWeight.w500,decoration: TextDecoration.underline),),
+           const SizedBox(height: 6,),
+            Text(
+              profile.bio,
+              style: const TextStyle(fontWeight: FontWeight.w500, fontSize: 16),
+            ),
+            const SizedBox(
+              height: 12,
+            ),
+           const Text("Animal Type:",style: TextStyle(fontSize: 15,fontWeight: FontWeight.w500,decoration: TextDecoration.underline),),
+           const SizedBox(height: 6,),
+            Row(
+              children: [
+                FaIcon(
+                  profile.type.name=="cat"?
+
+                  FontAwesomeIcons.cat:
+                  FontAwesomeIcons.dog
+                  ,size: 15,),
+               const  SizedBox(width: 10,),
+                Text(
+                  profile.type.name,
+                  style: const TextStyle(fontWeight: FontWeight.w400, fontSize: 14),
+                ),
+
+              ],
+            ),
+            const SizedBox(
+              height: 12,
+            ),
+
+
+          ],
+        ),
+        )
         ],
+      ),
+      bottomNavigationBar:  Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ElevatedButton(onPressed: () {
+              }, child: const Text("Start Chat now")),
       ),
     );
   }
